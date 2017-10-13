931177f29cc2eb9676c045c7a1bd67c87d05237e
Chuck Kang <chuckkang@gmail.com>
2017-10-12 17:59:01 -0700

    retro barcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Retro-Barcode';
+  arr = this.changeColor();
+  changeColor(): Array<string> {
+	  let arr: Array<string> = ["Azure", "Blue", "Crimson", "DarkSalmon", "Gainsboro", "Ivory", "LightGray", "Magenta", "OliveDrab", "SeaShell", "Tomato", "Yellow"];
+	  let newarr: Array<string> = [];
+	  var rnd: number;
+	  for (let i = 0; i < arr.length; i++) {
+		  rnd = Math.floor(Math.random() * (arr.length + 1));
+		  newarr.push(arr[rnd]);
+	  };
+	  return newarr;
+  }
+  ngOnInit() {
+	  this.changeColor();
+  }
 }
+
+
+
